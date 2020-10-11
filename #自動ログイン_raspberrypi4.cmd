db80cda364bda5866e93f83b34490564091ffbb9
ボクはお金で動くよ <70741260+black-triple-star@users.noreply.github.com>
2020-10-11 13:03:00 +0900

    1st

---
+++ #自動ログイン_raspberrypi4.cmd
@@ -0,0 +1,37 @@
+rem #####################################
+rem 改行コードがCR+LFでないと動作しません
+rem #####################################
+chcp 65001
+chcp 932
+
+
+rem AWS接続
+rem 評価環境(evaluation)1 
+rem ec2-50-17-231-192.compute-1.amazonaws.com
+rem 
+rem 評価環境(evaluation)8 
+rem ec2-184-73-182-41.compute-1.amazonaws.com
+rem 
+rem 評価環境(evaluation)9 
+rem ec2-107-20-199-63.compute-1.amazonaws.com
+rem 
+rem 評価環境(evaluation)12 
+rem ec2-50-17-221-160.compute-1.amazonaws.com
+
+set SERVER=ec2-50-17-231-192.compute-1.amazonaws.com
+set SERVER=192.168.0.127
+set SERVER=192.168.0.7
+
+set KEY_PATH=L:\NTT docomo リコメンド\鍵\id_rsa_forEC2
+
+rem #############################################################################################################
+
+rem パスワード認証（キーファイルなし）版
+rem echo connect '%SERVER% /ssh2 /auth=password /user=takashi.nakamura /passwd=4kTyrwKs'>%temp%\temp.ttl
+rem echo connect '%SERVER% /ssh2 /auth=password /user=root /passwd=qwerty1@'>%temp%\temp.ttl
+echo connect '%SERVER% /ssh2 /auth=password /user=takashi /passwd=r5i8t6u7y'>%temp%\temp.ttl
+
+rem キーファイルで認証版
+rem echo connect '%SERVER% /ssh2 /auth=publickey  /user=root /passwd="" /keyfile="%KEY_PATH%"'>%temp%\temp.ttl
+
+start /b C:\bin\teraterm\ttermpro.exe /M=%temp%\temp.ttl
